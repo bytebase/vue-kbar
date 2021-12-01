8770e57999fe40777b4bc1c99ea74d2fd3995d9a
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2021-12-01 17:27:36 +0800

    hide the panel when click outside

--- src/KBarAnimator.vue
+++ src/KBarAnimator.vue
@@ -1,28 +1,55 @@
 <template>
-  <div class="k-bar-animator" style="pointer-events: auto"><slot /></div>
+  <div ref="outerRef" class="k-bar-animator" style="pointer-events: auto">
+    <slot />
+  </div>
 </template>
 
-<script>
-import { defineComponent, watchEffect } from "vue";
+<script lang="ts">
+import { useEventListener } from "@vueuse/core";
+import { defineComponent, Ref, ref, watchEffect } from "vue";
+import { useKBarHandler } from "./useKBarHandler";
 import { useKBarState } from "./useKBarState";
 
 export default defineComponent({
   name: "KBarAnimator",
   setup() {
     const state = useKBarState();
+    const handler = useKBarHandler();
+
     watchEffect(() => {
       const { visibility } = state.value;
       if (visibility === "entering") {
         requestAnimationFrame(() => {
-          state.value.visibility = "visible";
+          handler.value.setVisibility("visible");
         });
       }
       if (visibility === "leaving") {
         requestAnimationFrame(() => {
-          state.value.visibility = "hidden";
+          handler.value.setVisibility("hidden");
         });
       }
     });
+
+    const outerRef = ref<HTMLDivElement | null>(null);
+    useClickOutside(outerRef, () => {
+      handler.value.hide();
+    });
+
+    return { outerRef };
   },
 });
+
+function useClickOutside(
+  domRef: Ref<HTMLElement | null>,
+  cb: (e: Event) => void
+) {
+  const handler = (e: Event) => {
+    if (domRef.value?.contains(e.target as any)) {
+      return;
+    }
+    cb(e);
+  };
+  useEventListener("mousedown", handler, true);
+  useEventListener("touchstart", handler, true);
+}
 </script>
