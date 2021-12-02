8014590d18c5e25011524c1d57077e92d4a0a9fb
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2021-12-02 15:01:59 +0800

    fix type complains

--- src/action/createAction.ts
+++ src/action/createAction.ts
@@ -4,9 +4,8 @@ function randomId() {
   return Math.random().toString(36).substring(2, 9);
 }
 
-export function createAction<T>(params: Omit<Action<T>, "id">) {
-  return {
-    id: randomId(),
-    ...params,
-  } as Action;
+export function createAction<T>(params: Partial<Action<T>>): Action<T> {
+  if (!params.id) params.id = randomId();
+
+  return params as Action<T>;
 }
