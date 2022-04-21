2a63273e940306f17500f2e9bdc0605fe50fcad6
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-04-21 10:43:28 +0800

    chore: tuning style

--- docs/.vitepress/theme/RenderResults.vue
+++ docs/.vitepress/theme/RenderResults.vue
@@ -52,7 +52,8 @@ export default defineComponent({
   padding: 8px 16px;
   font-size: 10px;
   text-transform: uppercase;
-  opacity: 0.5;
+  opacity: 0.8;
+  background-color: var(--a2);
 }
 .item {
   height: 60px;
