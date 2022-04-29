1096fa1ab3824417ae0671bc3717a885fcd3ceac
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-04-29 10:16:45 +0800

    docs: support vitepress SSG building

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vitepress";
 import { getMainSidebar } from "./toc";
+import { resolve } from "path";
+
+const PROJECT_ROOT = resolve(__dirname, "../../");
+const resolvePath = (rel: string) => resolve(PROJECT_ROOT, rel);
 
 export default defineConfig({
   title: "@bytebase/vue-kbar",
   description: "Extensible command+k interface for Vue 3 applications.",
+  base: '/vue-kbar',
+  outDir: resolvePath("./dist_docs"),
   themeConfig: {
     sidebar: {
       "/": getMainSidebar(),
--- docs/.vitepress/theme/KBarLayout.vue
+++ docs/.vitepress/theme/KBarLayout.vue
@@ -1,62 +1,76 @@
 <template>
-  <KBarProvider
-    :actions="initialActions"
-    :options="{ disabled, compare: compareAction }"
-  >
-    <KBarPortal>
-      <KBarPositioner style="z-index: 1000; background: var(--a3)">
-        <KBarAnimator
-          style="
-            max-width: calc(75 * var(--unit));
-            width: 100%;
-            background: var(--background);
-            color: var(--foreground);
-            border-radius: var(--unit);
-            overflow: hidden;
-            box-shadow: var(--shadow);
-          "
+  <template v-if="!!KBarProvider">
+    <component
+      :is="KBarProvider"
+      :actions="initialActions"
+      :options="{ disabled, compare: compareAction }"
+    >
+      <component :is="KBarPortal">
+        <component
+          :is="KBarPositioner"
+          style="z-index: 1000; background: var(--a3)"
         >
-          <KBarSearch
+          <component
+            :is="KBarAnimator"
             style="
-              padding: calc(2 * var(--unit)) calc(2 * var(--unit));
-              font-size: calc(2 * var(--unit));
+              max-width: calc(75 * var(--unit));
               width: 100%;
-              box-sizing: border-box;
-              outline: none;
-              border: none;
               background: var(--background);
               color: var(--foreground);
+              border-radius: var(--unit);
+              overflow: hidden;
+              box-shadow: var(--shadow);
             "
-          />
-          <RenderResults />
-        </KBarAnimator>
-      </KBarPositioner>
-    </KBarPortal>
+          >
+            <component
+              :is="KBarSearch"
+              style="
+                padding: calc(2 * var(--unit)) calc(2 * var(--unit));
+                font-size: calc(2 * var(--unit));
+                width: 100%;
+                box-sizing: border-box;
+                outline: none;
+                border: none;
+                background: var(--background);
+                color: var(--foreground);
+              "
+            />
+            <component :is="RenderResults" />
+          </component>
+        </component>
+      </component>
 
-    <Layout>
+      <Layout>
+        <template #navbar-search>
+          <DarkModeButton />
+        </template>
+      </Layout>
+    </component>
+  </template>
+
+  <template v-else>
+    <!-- <Layout>
       <template #navbar-search>
         <DarkModeButton />
       </template>
-    </Layout>
-  </KBarProvider>
+    </Layout> -->
+    asdf
+  </template>
 </template>
 
 <script>
-import { defineComponent, ref, watchEffect } from "vue";
+import {
+  defineComponent,
+  onBeforeMount,
+  ref,
+  shallowRef,
+  watchEffect,
+} from "vue";
 import DefaultTheme from "vitepress/theme";
 import { useRouter } from "vitepress";
 import slug from "slug";
 import { storeToRefs } from "pinia";
-import {
-  KBarProvider,
-  KBarPortal,
-  KBarPositioner,
-  KBarAnimator,
-  KBarSearch,
-  defineAction,
-} from "../../../src";
 import { useStore } from "../store.ts";
-import RenderResults from "./RenderResults.vue";
 import DarkModeButton from "./DarkModeButton.vue";
 import { getMainSidebar } from "../toc.ts";
 
@@ -67,40 +81,12 @@ export default defineComponent({
   components: {
     Layout,
     DarkModeButton,
-    KBarProvider,
-    KBarPortal,
-    KBarPositioner,
-    KBarAnimator,
-    KBarSearch,
-    RenderResults,
   },
   setup() {
     const store = useStore();
     const router = useRouter();
 
-    const docActions = getMainSidebar().flatMap((category) =>
-      category.children.map((page) =>
-        defineAction({
-          id: `kbar.documentation.${slug(page.text)}`,
-          name: page.text,
-          section: "Documentation",
-          perform: () => router.go(page.link),
-        })
-      )
-    );
-
-    const initialActions = [
-      defineAction({
-        id: "kbar.navigation.github",
-        name: "GitHub",
-        shortcut: ["g", "h"],
-        keywords: "sourcecode",
-        section: "Navigation",
-        perform: () =>
-          window.open("https://github.com/bytebase/vue-kbar", "_blank"),
-      }),
-      ...docActions,
-    ];
+    const initialActions = ref([]);
 
     const { disabled } = storeToRefs(store);
 
@@ -115,7 +101,57 @@ export default defineComponent({
       return ar - br;
     };
 
+    const KBarProvider = shallowRef(null);
+    const KBarPortal = shallowRef(null);
+    const KBarPositioner = shallowRef(null);
+    const KBarAnimator = shallowRef(null);
+    const KBarSearch = shallowRef(null);
+    const RenderResults = shallowRef(null);
+
+    onBeforeMount(() => {
+      Promise.all([import("../../../src"), import("./RenderResults.vue")]).then(
+        ([kbarModule, renderResultsModule]) => {
+          KBarProvider.value = kbarModule.KBarProvider;
+          KBarPortal.value = kbarModule.KBarPortal;
+          KBarPositioner.value = kbarModule.KBarPositioner;
+          KBarAnimator.value = kbarModule.KBarAnimator;
+          KBarSearch.value = kbarModule.KBarSearch;
+          RenderResults.value = renderResultsModule.default;
+          const { defineAction } = kbarModule;
+
+          const docActions = getMainSidebar().flatMap((category) =>
+            category.children.map((page) =>
+              defineAction({
+                id: `kbar.documentation.${slug(page.text)}`,
+                name: page.text,
+                section: "Documentation",
+                perform: () => router.go(page.link),
+              })
+            )
+          );
+          initialActions.value = [
+            defineAction({
+              id: "kbar.navigation.github",
+              name: "GitHub",
+              shortcut: ["g", "h"],
+              keywords: "sourcecode",
+              section: "Navigation",
+              perform: () =>
+                window.open("https://github.com/bytebase/vue-kbar", "_blank"),
+            }),
+            ...docActions,
+          ];
+        }
+      );
+    });
+
     return {
+      KBarProvider,
+      KBarPortal,
+      KBarPositioner,
+      KBarAnimator,
+      KBarSearch,
+      RenderResults,
       initialActions,
       disabled,
       compareAction,
